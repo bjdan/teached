824cf3ea644f2ad072e809d49b2949151c93c2c7
Kazimierz Filip <kazimierzfilipmail@gmail.com>
2020-05-16 16:07:33 +0200

    Add course and lesson authorization on findById

--- teachedapp/src/main/java/com/teachedapp/respository/CourseRepository.java
+++ teachedapp/src/main/java/com/teachedapp/respository/CourseRepository.java
@@ -2,8 +2,17 @@ package com.teachedapp.respository;
 
 import com.teachedapp.dao.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
+
+    @PostAuthorize("returnObject.get().teacher.account.login == authentication.name " +
+            "|| returnObject.get().student.account.login == authentication.name" +
+            "|| hasRole('ROLE_ADMIN')")
+    @Override
+    Optional<Course> findById(Integer id);
 }
