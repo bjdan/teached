ec102b58e9cbdacb64a39ab3e89a582e5dcda884
Kazimierz Filip <kazimierzfilipmail@gmail.com>
2020-06-17 00:13:26 +0200

    Add authorization to student repository

--- teachedapp/src/main/java/com/teachedapp/respository/StudentRepository.java
+++ teachedapp/src/main/java/com/teachedapp/respository/StudentRepository.java
@@ -1,18 +1,39 @@
 package com.teachedapp.respository;
 
 import com.teachedapp.dao.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    @PostAuthorize("hasRole('ROLE_STUDENT') and #id == authentication.principal.id" +
+            " or hasRole('ROLE_TEACHER') and returnObject.get().courses.![#this.teacher.id].contains(authentication.principal.id)" +
+            " or hasRole('ROLE_ADMIN')")
+    @Override
+    Optional<Student> findById(Integer id);
+
+    @Query("select s from Student s " +
+            "where s.id = ?#{ authentication.principal.id } " +
+            "or (?#{ security.hasRole('ROLE_TEACHER') ? 1 : 0 } = 1 " +
+            "and ?#{ authentication.principal.id } IN (select c.teacher from Course c where c.student = s.id)) " +
+            "or ?#{ security.hasRole('ROLE_ADMIN') ? 1 : 0 } = 1")
+    @Override
+    Page<Student> findAll(Pageable pageable);
+
     List<Student> findByAccount_EmailContainingIgnoreCase(String email);
+
     List<Student> findByAccount_LoginContainingIgnoreCase(String login);
+
     List<Student> findByAccount_PersonalData_FirstNameContainingIgnoreCaseAndAccount_PersonalData_LastNameContainingIgnoreCase(String firstName, String lastName);
+
     List<Student> findByAccount_PersonalData_Sex(Character sex);
 
     @Query("select s\n" +
